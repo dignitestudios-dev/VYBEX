df5f3521ba4cde7b09cec03a8c82ae0f5fcbaa80
Austin-seth <183656254+Austin-seth@users.noreply.github.com>
2025-06-18 01:03:49 -0700

    update:mockup

--- app/component/Live.js
+++ app/component/Live.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 export const Live = () => {
   return (
     <div className="flex items-center justify-center p-4 md:py-24">
-      <div className="lg:w-[95%]  border-[1px] border-[#252530AB]   rounded-[30px] bg-gradient-to-br from-40% from-[#040818]  via-75% via-[#0000FE] to-100% to-[#D0090B] md:h-[420px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center  p-8    md:gap-20  ">
+      <div className="lg:w-[95%]  border-[1px] border-[#252530AB]   rounded-[30px] bg-gradient-to-br from-40% from-[#040818]  via-75% via-[#0000FE] to-100% to-[#D0090B] md:h-[420px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center  p-8    md:gap-4  ">
         <div className="flex flex-col  lg:pt-20 ">
           <h3 className="text-[38px] md:text-[46px] font-[600] text-[#FFFFFF] md:leading-[53px] leading-[43px] ">
             Live Streaming Broadcast And Interact In <br />
@@ -14,8 +14,8 @@ export const Live = () => {
             </span>
           </h3>
         </div>
-        <div className="relative -top-28   lg:flex justify-center hidden ">
-          <Image src={livemockup} alt="" className="h-[680px] w-[500px]" />
+        <div className="relative xl:-top-36 -top-28   lg:flex justify-center hidden ">
+          <Image src={livemockup} alt="" className="xl:h-[680px]   xl:w-[400px]" />
         </div>
         <div className="lg:pt-20">
           <p className="text-[18px] font-[400] text-[#FFFFFF] pt-8">
